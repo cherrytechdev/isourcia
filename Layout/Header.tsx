5a028fd9d154a90f66409444502cd908336be5e3
Princy77 <fitiahary5@gmail.com>
2025-10-06 09:43:20 +0300

    .

--- Layout/Header.tsx
+++ Layout/Header.tsx
@@ -30,7 +30,7 @@ export default function Header(){
                                 viewBox="0 0 24 24" 
                                 fill="none" 
                                 stroke="currentColor" 
-                                strokeWidth="2" 
+                                strokeWidth="2"
                                 strokeLinecap="round" 
                                 strokeLinejoin="round" 
                                 className="transition duration-300 group-hover:rotate-45"><rect width="7" height="9" x="3" y="3" rx="1"/><rect width="7" height="5" x="14" y="3" rx="1"/><rect width="7" height="9" x="14" y="12" rx="1"/><rect width="7" height="5" x="3" y="16" rx="1"/></svg>
