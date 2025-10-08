a594fae02e2d1c151e907995859bed53dc6449e2
rakotomalalaheryniainatahina-code <rakotomalalaheryniainatahina@gmail.com>
2025-10-08 09:46:58 +0300

    8 oct | push

--- app/pages/Contact/page.tsx
+++ app/pages/Contact/page.tsx
@@ -1,11 +1,8 @@
 'use client'
 
 import ContactForm from "@/common/ContactForm";
-import { useFormStore } from "@/store/formStore";
-
 
 export default function Contact(){
-    const {values} = useFormStore()
     return(
         <section className="bg-gradient-to-br from-slate-900 via-purple-900 to-red-900">
             <ContactForm/>
